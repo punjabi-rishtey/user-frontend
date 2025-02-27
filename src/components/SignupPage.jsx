2a4207cc20ae1055833188654c86b7a49bab4f42
Akshat Jain <aibinnovations@gmail.com>
2025-02-27 13:40:52 +0530

    sigup page add preferences

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -16,6 +16,11 @@ const SignupPage = () => {
     religion: "",
     marital_status: "",
   });
+  const [preferences, setPreferences] = useState({
+    preference1: "",
+    preference2: "",
+    preference3: "",
+  });
   const { signup } = useAuth();
   const navigate = useNavigate();
   const [showPreferences, setShowPreferences] = useState(false);
@@ -24,6 +29,14 @@ const SignupPage = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
+      [name]: value,
+    });
+  };
+
+  const handlePreferenceChange = (e) => {
+    const { name, value } = e.target;
+    setPreferences({
+      ...preferences,
       [name]: value,
     });
   };
@@ -45,127 +58,185 @@ const SignupPage = () => {
 
       {/* Signup Form */}
       <div className="flex-grow flex items-center justify-center my-16">
-        {" "}
-        {/* Added margin */}
-        <div className="bg-[#F5EDE7] p-8 rounded-lg shadow-lg w-full max-w-md">
-          <h2
-            className="text-3xl mb-6 text-[#4F2F1D]"
-            style={{ fontFamily: "'Tiempos Headline', serif", fontWeight: 400 }}
-          >
-            Sign Up
-          </h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            {[
-              { label: "Name", name: "name", type: "text" },
-              { label: "Mobile", name: "mobile", type: "text" },
-              {
-                label: "Gender",
-                name: "gender",
-                type: "select",
-                options: [
-                  { value: "", label: "Select Gender" },
-                  { value: "male", label: "Male" },
-                  { value: "female", label: "Female" },
-                ],
-              },
-              { label: "Date of Birth", name: "dob", type: "date" },
-              {
-                label: "Religion",
-                name: "religion",
-                type: "select",
-                options: [
-                  { value: "", label: "Select Religion" },
-                  { value: "hindu", label: "Hindu" },
-                  { value: "sikh", label: "Sikh" },
-                  { value: "jain", label: "Jain" },
-                  { value: "buddhist", label: "Buddhist" },
-                ],
-              },
-              {
-                label: "Marital Status",
-                name: "marital_status",
-                type: "select",
-                options: [
-                  { value: "", label: "Select Marital Status" },
-                  { value: "never_married", label: "Never Married" },
-                  { value: "divorced", label: "Divorced" },
-                  { value: "widow_widower", label: "Widow/Widower" },
-                  { value: "awaiting_divorce", label: "Awaiting Divorce" },
-                  { value: "annulled", label: "Annulled" },
-                ],
-              },
-              { label: "Email", name: "email", type: "email" },
-              { label: "Password", name: "password", type: "password" },
-            ].map((field) => (
-              <div key={field.name}>
-                <label
-                  className="block text-[#6B4132] mb-2"
+        <div className="flex space-x-10">
+          <div className="bg-[#F5EDE7] p-8 rounded-lg shadow-lg" style={{ width: '450px' }}>
+            <h2
+              className="text-3xl mb-6 text-[#4F2F1D]"
+              style={{ fontFamily: "'Tiempos Headline', serif", fontWeight: 400 }}
+            >
+              Sign Up
+            </h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              {[
+                { label: "Name", name: "name", type: "text" },
+                { label: "Mobile", name: "mobile", type: "text" },
+                {
+                  label: "Gender",
+                  name: "gender",
+                  type: "select",
+                  options: [
+                    { value: "", label: "Select Gender" },
+                    { value: "male", label: "Male" },
+                    { value: "female", label: "Female" },
+                  ],
+                },
+                { label: "Date of Birth", name: "dob", type: "date" },
+                {
+                  label: "Religion",
+                  name: "religion",
+                  type: "select",
+                  options: [
+                    { value: "", label: "Select Religion" },
+                    { value: "hindu", label: "Hindu" },
+                    { value: "sikh", label: "Sikh" },
+                    { value: "jain", label: "Jain" },
+                    { value: "buddhist", label: "Buddhist" },
+                  ],
+                },
+                {
+                  label: "Marital Status",
+                  name: "marital_status",
+                  type: "select",
+                  options: [
+                    { value: "", label: "Select Marital Status" },
+                    { value: "never_married", label: "Never Married" },
+                    { value: "divorced", label: "Divorced" },
+                    { value: "widow_widower", label: "Widow/Widower" },
+                    { value: "awaiting_divorce", label: "Awaiting Divorce" },
+                    { value: "annulled", label: "Annulled" },
+                  ],
+                },
+                { label: "Email", name: "email", type: "email" },
+                { label: "Password", name: "password", type: "password" },
+              ].map((field) => (
+                <div key={field.name}>
+                  <label
+                    className="block text-[#6B4132] mb-2"
+                    style={{
+                      fontFamily: "'Modern Era', sans-serif",
+                      fontWeight: 400,
+                    }}
+                  >
+                    {field.label}
+                  </label>
+                  {field.type === "select" ? (
+                    <select
+                      name={field.name}
+                      value={formData[field.name]}
+                      onChange={handleChange}
+                      className="w-full p-3 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white"
+                      style={{
+                        fontFamily: "'Modern Era', sans-serif",
+                        fontWeight: 400,
+                      }}
+                      required
+                    >
+                      {field.options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                          {option.label}
+                        </option>
+                      ))}
+                    </select>
+                  ) : (
+                    <input
+                      type={field.type}
+                      name={field.name}
+                      value={formData[field.name]}
+                      onChange={handleChange}
+                      className="w-full p-3 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white"
+                      style={{
+                        fontFamily: "'Modern Era', sans-serif",
+                        fontWeight: 400,
+                      }}
+                      required
+                    />
+                  )}
+                </div>
+              ))}
+
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="bg-[#990000] hover:bg-[#800000] text-white font-bold py-2 px-6 rounded-lg transition duration-300"
                   style={{
                     fontFamily: "'Modern Era', sans-serif",
                     fontWeight: 400,
                   }}
                 >
-                  {field.label}
-                </label>
-                {field.type === "select" ? (
-                  <select
-                    name={field.name}
-                    value={formData[field.name]}
-                    onChange={handleChange}
-                    className="w-full p-3 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white"
-                    style={{
-                      fontFamily: "'Modern Era', sans-serif",
-                      fontWeight: 400,
-                    }}
-                    required
-                  >
-                    {field.options.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </select>
-                ) : (
-                  <input
-                    type={field.type}
-                    name={field.name}
-                    value={formData[field.name]}
-                    onChange={handleChange}
-                    className="w-full p-3 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white"
-                    style={{
-                      fontFamily: "'Modern Era', sans-serif",
-                      fontWeight: 400,
-                    }}
-                    required
-                  />
-                )}
-              </div>
-            ))}
-
-            <div className="flex justify-end">
+                  Sign Up
+                </button>
+                </div>
+            </form>
+            <div className="mt-6 text-center space-y-2">
               <button
-                type="submit"
-                className="bg-[#990000] hover:bg-[#800000] text-white font-bold py-2 px-6 rounded-lg transition duration-300"
+                className="text-[#4A4A4A] hover:text-[#2D2D2D] hover:underline transition duration-300"
+                onClick={() => navigate("/login")}
                 style={{
                   fontFamily: "'Modern Era', sans-serif",
                   fontWeight: 400,
                 }}
               >
-                Sign Up
+                Already have an account? Login
               </button>
             </div>
-          </form>
-          <div className="mt-6 text-center space-y-2">
-            <button
-              className="text-[#4A4A4A] hover:text-[#2D2D2D] hover:underline transition duration-300"
-              onClick={() => navigate("/login")}
-              style={{
-                fontFamily: "'Modern Era', sans-serif",
-                fontWeight: 400,
-              }}
-            >
-              Already have an account? Login
-            </button>
+          </div>
+
+          <div className="bg-[#F5EDE7] p-8 rounded-lg shadow-lg" style={{ width: '450px', height: '450px' }}>
+            <h2 className="text-3xl mb-6 text-[#4F2F1D]">Preferences</h2>
+            <form className="space-y-4">
+              {["preference1", "preference2", "preference3"].map((pref, index) => (
+                <div key={index} className="flex items-center space-x-4">
+                  <div>
+                    <label className="block text-[#6B4132] mb-2">Preference {index + 1}</label>
+                    <select
+                      name={pref}
+                      value={preferences[pref]}
+                      onChange={handlePreferenceChange}
+                      className="w-full p-3 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white"
+                      required
+                    >
+                      <option value="">Select Preference</option>
+                      <option value="age">Age</option>
+                      <option value="caste">Caste</option>
+                      <option value="height">Height</option>
+                      <option value="marital_status">Marital Status</option>
+                    </select>
+                  </div>
+                  {(preferences[pref] === "age" || preferences[pref] === "height") && (
+                    <div className="flex items-end space-x-2">
+                      <div>
+                        <label className="block text-[#6B4132] mb-2">Lower Than</label>
+                        <input type="number" className="w-full p-3 border border-[#6B4132] rounded-lg" />
+                      </div>
+                      <div>
+                        <label className="block text-[#6B4132] mb-2">Higher Than</label>
+                        <input type="number" className="w-full p-3 border border-[#6B4132] rounded-lg" />
+                      </div>
+                    </div>
+                  )}
+                  {preferences[pref] === "caste" && (
+                    <div>
+                      <select className="w-full p-3 mt-8 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white">
+                        <option value="">Select Caste</option>
+                        {["Sikh", "Hindu", "Jain", "Muslim", "Christian"].map(caste => (
+                          <option key={caste} value={caste}>{caste}</option>
+                        ))}
+                      </select>
+                    </div>
+                  )}
+                  {preferences[pref] === "marital_status" && (
+                    <div>
+                      <select className="w-full p-3 mt-8 border border-[#6B4132] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4F2F1D] bg-white">
+                        <option value="">Select Marital Status</option>
+                        {["Never Married", "Divorced", "Widow/Widower", "Awaiting Divorce", "Annulled"].map(status => (
+                          <option key={status} value={status.toLowerCase()}>{status}</option>
+                        ))}
+                      </select>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </form>
           </div>
         </div>
       </div>
@@ -180,7 +251,6 @@ const SignupPage = () => {
         />
       )}
 
-      {/* Footer */}
       <Footer />
     </div>
   );
