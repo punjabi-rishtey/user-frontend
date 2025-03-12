f33d31d2001a08216b4db2486e233da562029ad3
AIB Innovations <aibinnovations@gmail.com>
2025-03-12 16:54:21 +0530

    Updated Home Page Video

--- src/components/HeroWithHeader.jsx
+++ src/components/HeroWithHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import videoSrc from "../assets/video.mp4";
+import videoSrc from "../assets/WeddingVideomp4.mp4";
 import logoSrc from "../assets/logo.png";
 import profileIcon from "../assets/profile.png";
 import { useAuth } from "../context/AuthContext";
